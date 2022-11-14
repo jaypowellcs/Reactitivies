a652f1df1e779b8d22cacf13b4c910cd15d29516
Jay Powell <jay.powellcs@gmail.com>
2022-11-13 20:13:23 -0600

    edit/delete handlers

---
+++ Application/Activities/Edit.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Domain;
+using MediatR;
+using Persistence;
+
+namespace Application.Activities
+{
+    public class Edit
+    {
+        public class Command : IRequest 
+        {
+            public Activity Activity { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command>
+        {
+        private readonly DataContext context;
+        public IMapper Mapper { get; }
+            public Handler(DataContext context, IMapper mapper)
+            {
+            this.Mapper = mapper;
+            this.context = context;
+            }
+
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var activity = await this.context.Activities.FindAsync(request.Activity.Id);
+
+               this.Mapper.Map(request.Activity, activity); 
+
+                await this.context.SaveChangesAsync(); 
+
+                return Unit.Value; 
+            }
+        }
+    }
+}
