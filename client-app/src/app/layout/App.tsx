5a3c5e138bd3befa8c43d49eb2e5c0893aac6113
Jay Powell <jay.powellcs@gmail.com>
2022-12-05 19:10:26 -0600

    Activity Dashboard

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,11 +1,14 @@
 import React, { Fragment, useEffect, useState } from 'react';
 import axios from 'axios';
-import {Container, List} from 'semantic-ui-react';
+import {Container} from 'semantic-ui-react';
 import { Activity } from '../models/activity';
 import NavBar from './NavBar';
+import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
+  const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
+  const [editMode, setEditMode] = useState(false);
 
   useEffect(() => {
     axios.get<Activity[]>('http://localhost:5000/api/activities').then(response => {
@@ -13,18 +16,36 @@ function App() {
     })
   }, [])
 
+  function handleSelectActivity(id:string){
+    setSelectedActivity(activities.find(x =>x.id === id))
+  }
+
+  function handleCancelSelectActivity(){
+    setSelectedActivity(undefined);
+  }
+
+  function handleFormOpen(id?: string) {
+    id ? handleSelectActivity(id) : handleCancelSelectActivity();
+    setEditMode(true);
+  }
+
+  function handleFormClose() {
+    setEditMode(false);
+  }
 
   return (
     <>
-      <NavBar />
+      <NavBar openForm={handleFormOpen} />
       <Container style={{marginTop: '7em' }}>
-        <List>
-        {activities.map((activity: any) => (
-              <List.Item key={activity.id}>
-                {activity.title}
-              </List.Item>
-            ))}
-        </List>
+      <ActivityDashboard 
+      activities={activities}
+      selectedActivity={selectedActivity}
+      selectActivity={handleSelectActivity}
+      cancelSelectActivity={handleCancelSelectActivity}
+      editMode={editMode}
+      openForm={handleFormOpen}
+      closeForm={handleFormClose}
+      />
       </Container>
     </>
   );
